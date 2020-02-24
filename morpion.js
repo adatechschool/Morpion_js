232296e26fc56cd266bca0af6c16874aa58be37f
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-02-24 16:32:11 +0100

    lol

--- morpion.js
+++ morpion.js
@@ -1,9 +1,11 @@
 let valeurs = new Array();
 let j = 1;
 let tour = 1;
-let joueur1 = 0;
-let joueur2 = 0;
-
+let joueur1 = new Array();
+let joueur2 = new Array();
+let combos_gagnants = new Array();
+let combos = new Array();
+ 
 for (let i = 1; i<=9; i++)
 {
     valeurs[i] = j*i;
@@ -21,18 +23,26 @@ for (let k = 1; k<10; k++)
 {
     id = "case_" + k;
     cases[k] = document.getElementById(id);
-    cases[k].addEventListener("click", click_case(cases[k]));
+    cases[k].addEventListener("click", function(){
+        click_case(cases[k]);
+    });
 }
 
 function click_case(lacase)
 {
+    lacase.classList.remove('blue');
+    lacase.classList.remove('red');
     if (tour == 1)
     {
         lacase.classList.add("blue");
+        joueur1.push(lacase);
+        console.log(joueur1);
     }
     else
     {
         lacase.classList.add('red');
+        joueur2.push(lacase);
+        console.log(joueur2);
     }
     changer_tour();
 }
@@ -43,20 +53,6 @@ console.log(cases);
 alert("Bienvenue");
 alert("On va jouer au morpion !!");
 alert("Joueur 1, tu commence !");
-
-// function click_case(case)
-// {
-//         case.addEventListener("click", function(){
-//             if (tour == 1)
-//             {
-//                 case.classList.add("red");
-//             }
-//             else
-//             {
-//                 case.classList.add("blue");
-//             }
-//         });
-// }
 
 function changer_tour()
 {
