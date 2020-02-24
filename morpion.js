381295b2e2b73cfbf6a1b14c873a80d4136d69af
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-02-24 15:56:09 +0100

    le commit le plus explicite de la galaxie*

--- morpion.js
+++ morpion.js
@@ -1,13 +1,62 @@
-for (let i = 1; i<4; i++)
-{
-    
-}
-
-
+let valeurs = new Array();
+let j = 1;
 let tour = 1;
 let joueur1 = 0;
 let joueur2 = 0;
 
+for (let i = 1; i<=9; i++)
+{
+    valeurs[i] = j*i;
+    if (i%3 == 0)
+    {
+        j *= 10;
+    }
+}
+
+console.log(valeurs);
+
+let id = "";
+let cases = new Array();
+for (let k = 1; k<10; k++)
+{
+    id = "case_" + k;
+    cases[k] = document.getElementById(id);
+    cases[k].addEventListener("click", click_case(cases[k]));
+}
+
+function click_case(lacase)
+{
+    if (tour == 1)
+    {
+        lacase.classList.add("blue");
+    }
+    else
+    {
+        lacase.classList.add('red');
+    }
+    changer_tour();
+}
+
+console.log(cases);
+
+
+alert("Bienvenue");
+alert("On va jouer au morpion !!");
+alert("Joueur 1, tu commence !");
+
+// function click_case(case)
+// {
+//         case.addEventListener("click", function(){
+//             if (tour == 1)
+//             {
+//                 case.classList.add("red");
+//             }
+//             else
+//             {
+//                 case.classList.add("blue");
+//             }
+//         });
+// }
 
 function changer_tour()
 {
