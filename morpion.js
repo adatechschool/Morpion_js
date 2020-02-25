e01b521d8b9c32a0ee8c89ff86ff3076a8aed44f
FlorianBouteille <34886355+FlorianBouteille@users.noreply.github.com>
2020-02-25 17:33:41 +0100

    ça marche et ya du star wars

--- morpion.js
+++ morpion.js
@@ -1,58 +1,90 @@
+alert("Bienvenue");
+alert("On va jouer au morpion !!");
+alert("Joueur 1, tu commence !");
+
 let valeurs = new Array();
-let j = 1;
+valeurs = [1, 2, 3, 10, 20, 30, 100, 200, 300];
+console.log(valeurs);
+
 let tour = 1;
 let joueur1 = new Array();
 let joueur2 = new Array();
 let combos_gagnants = new Array();
-let combos = new Array();
- 
-for (let i = 1; i<=9; i++)
-{
-    valeurs[i] = j*i;
-    if (i%3 == 0)
-    {
-        j *= 10;
-    }
-}
+combos_gagnants = [6, 60, 600, 111, 222, 333, 123, 321];
+console.log(combos_gagnants);
 
-console.log(valeurs);
-
-let id = "";
 let cases = new Array();
 for (let k = 1; k<10; k++)
 {
-    id = "case_" + k;
+    let id = "case_" + k;
     cases[k] = document.getElementById(id);
-    cases[k].addEventListener("click", function(){
-        click_case(cases[k]);
+    cases[k].addEventListener("click",  function(){
+        click_case(cases[k], k);
     });
 }
 
-function click_case(lacase)
+function check_player_combos(player)
 {
+    let combos_player = new Array();
+    combos_player.push(player[0] + player[1] + player[2]);
+    combos_player.push(player[0] + player[1] + player[3]);
+    combos_player.push(player[0] + player[1] + player[4]);
+    combos_player.push(player[0] + player[2] + player[3]);
+    combos_player.push(player[0] + player[2] + player[4]);
+    combos_player.push(player[0] + player[3] + player[4]);
+    combos_player.push(player[1] + player[2] + player[3]);
+    combos_player.push(player[1] + player[2] + player[4]);
+    combos_player.push(player[1] + player[3] + player[4]);
+    combos_player.push(player[2] + player[3] + player[4]);
+    console.log(combos_player);
+    return combos_player;
+}
+
+function check_win(player)
+{
+    let combos = check_player_combos(player);
+    for (let i = 0; i<10; i++)
+    {
+        if (combos_gagnants.includes(combos[i]))
+        {
+            alert("Le joueur " + tour + " remporte la partie !");
+            document.location.reload(true);
+        }
+    }
+    if (joueur1.length == 5)
+    {
+        alert("EGALITE, vous etes trop forts.");
+        document.location.reload(true);
+    }
+    return false;
+}
+
+function click_case(lacase, index)
+{
+    if (lacase.classList.contains("red") || lacase.classList.contains("blue"))
+    {
+        alert("cette case est déja prise !");
+        return;
+    }
+    let value = valeurs[index - 1];
     lacase.classList.remove('blue');
     lacase.classList.remove('red');
     if (tour == 1)
     {
         lacase.classList.add("blue");
-        joueur1.push(lacase);
+        joueur1.push(value);
         console.log(joueur1);
+        check_win(joueur1);
     }
     else
     {
         lacase.classList.add('red');
-        joueur2.push(lacase);
+        joueur2.push(value);
         console.log(joueur2);
+        check_win(joueur2);
     }
     changer_tour();
 }
-
-console.log(cases);
-
-
-alert("Bienvenue");
-alert("On va jouer au morpion !!");
-alert("Joueur 1, tu commence !");
 
 function changer_tour()
 {
@@ -67,3 +99,8 @@ function changer_tour()
         alert("c'est le tour du joueur 1");
     }
 }
+
+// let canvas = document.getElementById("canvas");
+// let ctx = canvas.getContext("2d");
+// ctx.font = "56px serif";
+// ctx.fillText("Attrapez les tous", 100, 100);
